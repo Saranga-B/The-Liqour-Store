bc9f53a02cedc940aea8ff01c9aa94cb7641258d
Saranga-B <bandarasaranga@icloud.com>
2023-03-21 22:54:21 +0530

    Custon logger

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config';
+import logger from "./utils/logger";
 
 const app = express();
 const PORT=process.env.PORT || "8090";
@@ -14,5 +15,6 @@ app.get("/",(req,res,next)=>{
 });
 
 app.listen(PORT, () => {
+    logger.info("This is testing")
     console.log('Server is up and running on PORT ${PORT}');
 });
