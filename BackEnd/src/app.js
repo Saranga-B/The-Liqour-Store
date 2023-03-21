6aaa23523a6c1778db508fab8514d5a242f7f43d
Saranga-B <bandarasaranga@icloud.com>
2023-03-21 13:32:40 +0530

    Server Configuration

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,5 +1,18 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import 'dotenv/config';
 
-dotenv.config();
+const app = express();
+const PORT=process.env.PORT || "8090";
+
+app.use(cors());
+app.use(express.json({limit:"20mb"}))
+
+app.get("/",(req,res,next)=>{
+    res.send("<h1>Liqour Store</h2>");
+    next;
+});
+
+app.listen(PORT, () => {
+    console.log('Server is up and running on PORT ${PORT}');
+});
